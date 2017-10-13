c17bf35e8d2750c858ee3b1b98a8ac9cbc24dd88
Yuri Lemes <yurilemes2@gmail.com>
2017-10-13 18:09:06 -0300

    Desenvolvendo Objetos BlocoD

---
+++ src/main/java/br/com/samuelweb/efd/contribuicoes/registros/blocoD/RegistroD010.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.com.samuelweb.efd.contribuicoes.registros.blocoD;
+
+import java.util.List;
+
+/**
+ * @author Yuri Lemes
+ *
+ */
+public class RegistroD010 {
+
+	private final String reg = "D010";
+	private String cnpj;
+	private List<RegistroD100> registroD100;
+	private List<RegistroD200> registroD200;
+	private List<RegistroD300> registroD300;
+	private List<RegistroD350> registroD350;
+	private List<RegistroD500> registroD500;
+	private List<RegistroD600> registroD600;
+}
---
+++ src/main/java/br/com/samuelweb/efd/contribuicoes/registros/blocoD/RegistroD350.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package br.com.samuelweb.efd.contribuicoes.registros.blocoD;
+
+/**
+ * @author Yuri Lemes
+ *
+ */
+public class RegistroD350 {
+
+	private final String reg = "D350";
+	private String cod_mod;
+	private String ecf_mod;
+	private String ecf_fab;
+	private String dt_doc;
+	private String cro;
+	private String crz;
+	private String num_coo_fin;
+	private String gt_fin;
+	private String vl_brt;
+	private String cst_pis;
+	private String vl_bc_pis;
+	private String aliq_pis_percentual;
+	private String quant_bc_pis;
+	private String aliq_pis_reais;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+}

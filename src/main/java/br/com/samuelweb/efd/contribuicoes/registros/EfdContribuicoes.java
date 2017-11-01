0a1d6a658ab35bcb82bac0e3ccb545dc22f7eb3f
Yuri Lemes <yurilemes2@gmail.com>
2017-11-01 08:35:39 -0200

    Finalizado Contadores

---
+++ src/main/java/br/com/samuelweb/efd/contribuicoes/registros/EfdContribuicoes.java
@@ -0,0 +1,186 @@
+/**
+ * 
+ */
+package br.com.samuelweb.efd.contribuicoes.registros;
+
+import br.com.samuelweb.efd.contribuicoes.registros.bloco0.Bloco0;
+import br.com.samuelweb.efd.contribuicoes.registros.bloco1.Bloco1;
+import br.com.samuelweb.efd.contribuicoes.registros.bloco9.Bloco9;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoA.BlocoA;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoC.BlocoC;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoD.BlocoD;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoF.BlocoF;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoI.BlocoI;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoM.BlocoM;
+import br.com.samuelweb.efd.contribuicoes.registros.blocoP.BlocoP;
+
+/**
+ * @author Yuri Lemes
+ *
+ */
+public class EfdContribuicoes {
+
+	private Bloco0 bloco0;
+	private Bloco1 bloco1;
+	private Bloco9 bloco9;
+	private BlocoA blocoA;
+	private BlocoC blocoC;
+	private BlocoD blocoD;
+	private BlocoF blocoF;
+	private BlocoI blocoI;
+	private BlocoM blocoM;
+	private BlocoP blocoP;
+
+	// TODO Contadores
+
+	/**
+	 * @return the bloco0
+	 */
+	public Bloco0 getBloco0() {
+		return bloco0;
+	}
+
+	/**
+	 * @param bloco0
+	 *            the bloco0 to set
+	 */
+	public void setBloco0(Bloco0 bloco0) {
+		this.bloco0 = bloco0;
+	}
+
+	/**
+	 * @return the bloco1
+	 */
+	public Bloco1 getBloco1() {
+		return bloco1;
+	}
+
+	/**
+	 * @param bloco1
+	 *            the bloco1 to set
+	 */
+	public void setBloco1(Bloco1 bloco1) {
+		this.bloco1 = bloco1;
+	}
+
+	/**
+	 * @return the bloco9
+	 */
+	public Bloco9 getBloco9() {
+		return bloco9;
+	}
+
+	/**
+	 * @param bloco9
+	 *            the bloco9 to set
+	 */
+	public void setBloco9(Bloco9 bloco9) {
+		this.bloco9 = bloco9;
+	}
+
+	/**
+	 * @return the blocoA
+	 */
+	public BlocoA getBlocoA() {
+		return blocoA;
+	}
+
+	/**
+	 * @param blocoA
+	 *            the blocoA to set
+	 */
+	public void setBlocoA(BlocoA blocoA) {
+		this.blocoA = blocoA;
+	}
+
+	/**
+	 * @return the blocoC
+	 */
+	public BlocoC getBlocoC() {
+		return blocoC;
+	}
+
+	/**
+	 * @param blocoC
+	 *            the blocoC to set
+	 */
+	public void setBlocoC(BlocoC blocoC) {
+		this.blocoC = blocoC;
+	}
+
+	/**
+	 * @return the blocoD
+	 */
+	public BlocoD getBlocoD() {
+		return blocoD;
+	}
+
+	/**
+	 * @param blocoD
+	 *            the blocoD to set
+	 */
+	public void setBlocoD(BlocoD blocoD) {
+		this.blocoD = blocoD;
+	}
+
+	/**
+	 * @return the blocoF
+	 */
+	public BlocoF getBlocoF() {
+		return blocoF;
+	}
+
+	/**
+	 * @param blocoF
+	 *            the blocoF to set
+	 */
+	public void setBlocoF(BlocoF blocoF) {
+		this.blocoF = blocoF;
+	}
+
+	/**
+	 * @return the blocoI
+	 */
+	public BlocoI getBlocoI() {
+		return blocoI;
+	}
+
+	/**
+	 * @param blocoI
+	 *            the blocoI to set
+	 */
+	public void setBlocoI(BlocoI blocoI) {
+		this.blocoI = blocoI;
+	}
+
+	/**
+	 * @return the blocoM
+	 */
+	public BlocoM getBlocoM() {
+		return blocoM;
+	}
+
+	/**
+	 * @param blocoM
+	 *            the blocoM to set
+	 */
+	public void setBlocoM(BlocoM blocoM) {
+		this.blocoM = blocoM;
+	}
+
+	/**
+	 * @return the blocoP
+	 */
+	public BlocoP getBlocoP() {
+		return blocoP;
+	}
+
+	/**
+	 * @param blocoP
+	 *            the blocoP to set
+	 */
+	public void setBlocoP(BlocoP blocoP) {
+		this.blocoP = blocoP;
+	}
+
+}
---
+++ src/main/java/br/com/samuelweb/efd/contribuicoes/registros/contadores/ContadoresBloco0.java
@@ -0,0 +1,243 @@
+/**
+ * 
+ */
+package br.com.samuelweb.efd.contribuicoes.registros.contadores;
+
+import br.com.samuelweb.efd.contribuicoes.registros.bloco0.Bloco0Enum;
+
+/**
+ * @author Yuri Lemes
+ *
+ */
+public class ContadoresBloco0 {
+
+	// BLOCO 0
+	private int contRegistro0000 = 0;
+	private int contRegistro0001 = 0;
+	private int contRegistro0035 = 0;
+	private int contRegistro0100 = 0;
+	private int contRegistro0110 = 0;
+	private int contRegistro0111 = 0;
+	private int contRegistro0120 = 0;
+	private int contRegistro0140 = 0;
+	private int contRegistro0145 = 0;
+	private int contRegistro0150 = 0;
+	private int contRegistro0190 = 0;
+	private int contRegistro0200 = 0;
+	private int contRegistro0205 = 0;
+	private int contRegistro0206 = 0;
+	private int contRegistro0208 = 0;
+	private int contRegistro0400 = 0;
+	private int contRegistro0450 = 0;
+	private int contRegistro0500 = 0;
+	private int contRegistro0600 = 0;
+	private int contRegistro0990 = 0;
+
+	public void incrementar(Bloco0Enum registroBloco0) {
+
+		contRegistro0990++;
+
+		switch (registroBloco0) {
+			case Registro0000:
+				contRegistro0000++;
+				break;
+			case Registro0001:
+				contRegistro0001++;
+				break;
+			case Registro0035:
+				contRegistro0035++;
+				break;
+			case Registro0100:
+				contRegistro0100++;
+				break;
+			case Registro0110:
+				contRegistro0110++;
+				break;
+			case Registro0111:
+				contRegistro0111++;
+				break;
+			case Registro0120:
+				contRegistro0120++;
+				break;
+			case Registro0140:
+				contRegistro0140++;
+				break;
+			case Registro0145:
+				contRegistro0145++;
+				break;
+			case Registro0150:
+				contRegistro0150++;
+				break;
+			case Registro0190:
+				contRegistro0190++;
+				break;
+			case Registro0200:
+				contRegistro0200++;
+				break;
+			case Registro0205:
+				contRegistro0205++;
+				break;
+			case Registro0206:
+				contRegistro0206++;
+				break;
+			case Registro0208:
+				contRegistro0208++;
+				break;
+			case Registro0400:
+				contRegistro0400++;
+				break;
+			case Registro0450:
+				contRegistro0450++;
+				break;
+			case Registro0500:
+				contRegistro0500++;
+				break;
+			case Registro0600:
+				contRegistro0600++;
+				break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * @return the contRegistro0000
+	 */
+	public int getContRegistro0000() {
+		return contRegistro0000;
+	}
+
+	/**
+	 * @return the contRegistro0001
+	 */
+	public int getContRegistro0001() {
+		return contRegistro0001;
+	}
+
+	/**
+	 * @return the contRegistro0035
+	 */
+	public int getContRegistro0035() {
+		return contRegistro0035;
+	}
+
+	/**
+	 * @return the contRegistro0100
+	 */
+	public int getContRegistro0100() {
+		return contRegistro0100;
+	}
+
+	/**
+	 * @return the contRegistro0110
+	 */
+	public int getContRegistro0110() {
+		return contRegistro0110;
+	}
+
+	/**
+	 * @return the contRegistro0111
+	 */
+	public int getContRegistro0111() {
+		return contRegistro0111;
+	}
+
+	/**
+	 * @return the contRegistro0120
+	 */
+	public int getContRegistro0120() {
+		return contRegistro0120;
+	}
+
+	/**
+	 * @return the contRegistro0140
+	 */
+	public int getContRegistro0140() {
+		return contRegistro0140;
+	}
+
+	/**
+	 * @return the contRegistro0145
+	 */
+	public int getContRegistro0145() {
+		return contRegistro0145;
+	}
+
+	/**
+	 * @return the contRegistro0150
+	 */
+	public int getContRegistro0150() {
+		return contRegistro0150;
+	}
+
+	/**
+	 * @return the contRegistro0190
+	 */
+	public int getContRegistro0190() {
+		return contRegistro0190;
+	}
+
+	/**
+	 * @return the contRegistro0200
+	 */
+	public int getContRegistro0200() {
+		return contRegistro0200;
+	}
+
+	/**
+	 * @return the contRegistro0205
+	 */
+	public int getContRegistro0205() {
+		return contRegistro0205;
+	}
+
+	/**
+	 * @return the contRegistro0206
+	 */
+	public int getContRegistro0206() {
+		return contRegistro0206;
+	}
+
+	/**
+	 * @return the contRegistro0208
+	 */
+	public int getContRegistro0208() {
+		return contRegistro0208;
+	}
+
+	/**
+	 * @return the contRegistro0400
+	 */
+	public int getContRegistro0400() {
+		return contRegistro0400;
+	}
+
+	/**
+	 * @return the contRegistro0450
+	 */
+	public int getContRegistro0450() {
+		return contRegistro0450;
+	}
+
+	/**
+	 * @return the contRegistro0500
+	 */
+	public int getContRegistro0500() {
+		return contRegistro0500;
+	}
+
+	/**
+	 * @return the contRegistro0600
+	 */
+	public int getContRegistro0600() {
+		return contRegistro0600;
+	}
+
+	/**
+	 * @return the contRegistro0990
+	 */
+	public int getContRegistro0990() {
+		return contRegistro0990;
+	}
+
+}
